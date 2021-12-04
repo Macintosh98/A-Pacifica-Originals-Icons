69d7ef7d3d35bc5c22652260c4f0f5dd106786b2
Macintosh98 <abhishekzambare@gmail.com>
2021-12-04 18:55:06 +0530

    reboot

---
+++ src/colors/change.sh
@@ -0,0 +1,239 @@
+#!/bin/bash
+
+
+#100,300,900
+
+# @define-color STRAWBERRY_100 #ff8c82;
+# @define-color STRAWBERRY_300 #ed5353;
+# @define-color STRAWBERRY_900 #7a0000;
+
+# sed -i 's/efdfc4/ff8c82/g' elementosh-STRAWBERRY/places/{16,24,32,48,64,128}/*
+# sed -i 's/e7c591/ed5353/g' elementosh-STRAWBERRY/places/{16,24,32,48,64,128}/*
+# sed -i 's/7e5514/7a0000/g' elementosh-STRAWBERRY/places/{16,24,32,48,64,128}/*
+
+# sed -i 's/efdfc4/ff8c82/g' elementosh-STRAWBERRY/actions/{16,24,32,48,64}/*
+# sed -i 's/e7c591/ed5353/g' elementosh-STRAWBERRY/actions/{16,24,32,48,64}/*
+# sed -i 's/7e5514/7a0000/g' elementosh-STRAWBERRY/actions/{16,24,32,48,64}/*
+
+# sed -i 's/e9e9e9/ff8c82/g' elementosh-STRAWBERRY/apps/{16,24,32,48,64,128}/*
+# sed -i 's/b4b4b4/ed5353/g' elementosh-STRAWBERRY/apps/{16,24,32,48,64,128}/*
+# sed -i 's/6c6c6c/7a0000/g' elementosh-STRAWBERRY/apps/{16,24,32,48,64,128}/*
+
+cp -r 64 elementosh-STRAWBERRY/apps
+gtk-update-icon-cache elementosh-STRAWBERRY
+
+# @define-color ORANGE_100 #ffc27d;
+# @define-color ORANGE_300 #ffa154;
+# @define-color ORANGE_900 #a62100;
+
+# sed -i 's/efdfc4/ffc27d/g' elementosh-ORANGE/places/{16,24,32,48,64,128}/*
+# sed -i 's/e7c591/ffa154/g' elementosh-ORANGE/places/{16,24,32,48,64,128}/*
+# sed -i 's/7e5514/a62100/g' elementosh-ORANGE/places/{16,24,32,48,64,128}/*
+
+# sed -i 's/efdfc4/ffc27d/g' elementosh-ORANGE/actions/{16,24,32,48,64}/*
+# sed -i 's/e7c591/ffa154/g' elementosh-ORANGE/actions/{16,24,32,48,64}/*
+# sed -i 's/7e5514/a62100/g' elementosh-ORANGE/actions/{16,24,32,48,64}/*
+
+# sed -i 's/e9e9e9/ffc27d/g' elementosh-ORANGE/apps/{16,24,32,48,64,128}/*
+# sed -i 's/b4b4b4/ffa154/g' elementosh-ORANGE/apps/{16,24,32,48,64,128}/*
+# sed -i 's/6c6c6c/a62100/g' elementosh-ORANGE/apps/{16,24,32,48,64,128}/*
+
+cp -r 64 elementosh-ORANGE/apps
+gtk-update-icon-cache elementosh-ORANGE
+
+# @define-color BANANA_100 #fff394;
+# @define-color BANANA_300 #ffe16b;
+# @define-color BANANA_900 #ad5f00;
+
+# sed -i 's/efdfc4/fff394/g' elementosh-BANANA/places/{16,24,32,48,64,128}/*
+# sed -i 's/e7c591/ffe16b/g' elementosh-BANANA/places/{16,24,32,48,64,128}/*
+# sed -i 's/7e5514/ad5f00/g' elementosh-BANANA/places/{16,24,32,48,64,128}/*
+
+# sed -i 's/efdfc4/fff394/g' elementosh-BANANA/actions/{16,24,32,48,64}/*
+# sed -i 's/e7c591/ffe16b/g' elementosh-BANANA/actions/{16,24,32,48,64}/*
+# sed -i 's/7e5514/ad5f00/g' elementosh-BANANA/actions/{16,24,32,48,64}/*
+
+# sed -i 's/e9e9e9/fff394/g' elementosh-BANANA/apps/{16,24,32,48,64,128}/*
+# sed -i 's/b4b4b4/ffe16b/g' elementosh-BANANA/apps/{16,24,32,48,64,128}/*
+# sed -i 's/6c6c6c/ad5f00/g' elementosh-BANANA/apps/{16,24,32,48,64,128}/*
+
+cp -r 64 elementosh-BANANA/apps
+gtk-update-icon-cache elementosh-BANANA
+
+
+# @define-color LIME_100 #d1ff82;
+# @define-color LIME_300 #9bdb4d;
+# @define-color LIME_900 #206b00;
+
+# sed -i 's/efdfc4/d1ff82/g' elementosh-LIME/places/{16,24,32,48,64,128}/*
+# sed -i 's/e7c591/9bdb4d/g' elementosh-LIME/places/{16,24,32,48,64,128}/*
+# sed -i 's/7e5514/206b00/g' elementosh-LIME/places/{16,24,32,48,64,128}/*
+
+# sed -i 's/efdfc4/d1ff82/g' elementosh-LIME/actions/{16,24,32,48,64}/*
+# sed -i 's/e7c591/9bdb4d/g' elementosh-LIME/actions/{16,24,32,48,64}/*
+# sed -i 's/7e5514/206b00/g' elementosh-LIME/actions/{16,24,32,48,64}/*
+
+# sed -i 's/e9e9e9/d1ff82/g' elementosh-LIME/apps/{16,24,32,48,64,128}/*
+# sed -i 's/b4b4b4/9bdb4d/g' elementosh-LIME/apps/{16,24,32,48,64,128}/*
+# sed -i 's/6c6c6c/206b00/g' elementosh-LIME/apps/{16,24,32,48,64,128}/*
+
+cp -r 64 elementosh-LIME/apps
+gtk-update-icon-cache elementosh-LIME
+
+
+# @define-color MINT_100 #89ffdd;
+# @define-color MINT_300 #43d6b5;
+# @define-color MINT_900 #007367;
+
+# sed -i 's/efdfc4/89ffdd/g' elementosh-MINT/places/{16,24,32,48,64,128}/*
+# sed -i 's/e7c591/43d6b5/g' elementosh-MINT/places/{16,24,32,48,64,128}/*
+# sed -i 's/7e5514/007367/g' elementosh-MINT/places/{16,24,32,48,64,128}/*
+
+# sed -i 's/efdfc4/89ffdd/g' elementosh-MINT/actions/{16,24,32,48,64}/*
+# sed -i 's/e7c591/43d6b5/g' elementosh-MINT/actions/{16,24,32,48,64}/*
+# sed -i 's/7e5514/007367/g' elementosh-MINT/actions/{16,24,32,48,64}/*
+
+# sed -i 's/e9e9e9/89ffdd/g' elementosh-MINT/apps/{16,24,32,48,64,128}/*
+# sed -i 's/b4b4b4/43d6b5/g' elementosh-MINT/apps/{16,24,32,48,64,128}/*
+# sed -i 's/6c6c6c/007367/g' elementosh-MINT/apps/{16,24,32,48,64,128}/*
+
+cp -r 64 elementosh-MINT/apps
+gtk-update-icon-cache elementosh-MINT
+
+
+# @define-color BLUEBERRY_100 #8cd5ff;
+# @define-color BLUEBERRY_300 #64baff;
+# @define-color BLUEBERRY_900 #002e99;
+
+# sed -i 's/efdfc4/8cd5ff/g' elementosh-BLUEBERRY/places/{16,24,32,48,64,128}/*
+# sed -i 's/e7c591/64baff/g' elementosh-BLUEBERRY/places/{16,24,32,48,64,128}/*
+# sed -i 's/7e5514/002e99/g' elementosh-BLUEBERRY/places/{16,24,32,48,64,128}/*
+
+# sed -i 's/efdfc4/8cd5ff/g' elementosh-BLUEBERRY/actions/{16,24,32,48,64}/*
+# sed -i 's/e7c591/64baff/g' elementosh-BLUEBERRY/actions/{16,24,32,48,64}/*
+# sed -i 's/7e5514/002e99/g' elementosh-BLUEBERRY/actions/{16,24,32,48,64}/*
+
+# sed -i 's/e9e9e9/8cd5ff/g' elementosh-BLUEBERRY/apps/{16,24,32,48,64,128}/*
+# sed -i 's/b4b4b4/64baff/g' elementosh-BLUEBERRY/apps/{16,24,32,48,64,128}/*
+# sed -i 's/6c6c6c/002e99/g' elementosh-BLUEBERRY/apps/{16,24,32,48,64,128}/*
+
+cp -r 64 elementosh-BLUEBERRY/apps
+gtk-update-icon-cache elementosh-BLUEBERRY
+# @define-color BUBBLEGUM_100 #fe9ab8;
+# @define-color BUBBLEGUM_300 #f4679d;
+# @define-color BUBBLEGUM_900 #910e38;
+
+# sed -i 's/efdfc4/fe9ab8/g' elementosh-BUBBLEGUM/places/{16,24,32,48,64,128}/*
+# sed -i 's/e7c591/f4679d/g' elementosh-BUBBLEGUM/places/{16,24,32,48,64,128}/*
+# sed -i 's/7e5514/910e38/g' elementosh-BUBBLEGUM/places/{16,24,32,48,64,128}/*
+
+# sed -i 's/efdfc4/fe9ab8/g' elementosh-BUBBLEGUM/actions/{16,24,32,48,64}/*
+# sed -i 's/e7c591/f4679d/g' elementosh-BUBBLEGUM/actions/{16,24,32,48,64}/*
+# sed -i 's/7e5514/910e38/g' elementosh-BUBBLEGUM/actions/{16,24,32,48,64}/*
+
+# sed -i 's/e9e9e9/fe9ab8/g' elementosh-BUBBLEGUM/apps/{16,24,32,48,64,128}/*
+# sed -i 's/b4b4b4/f4679d/g' elementosh-BUBBLEGUM/apps/{16,24,32,48,64,128}/*
+# sed -i 's/6c6c6c/910e38/g' elementosh-BUBBLEGUM/apps/{16,24,32,48,64,128}/*
+
+cp -r 64 elementosh-BUBBLEGUM/apps
+gtk-update-icon-cache elementosh-BUBBLEGUM
+
+
+# @define-color GRAPE_100 #e4c6fa;
+# @define-color GRAPE_300 #cd9ef7;
+# @define-color GRAPE_900 #452981;
+
+# sed -i 's/efdfc4/e4c6fa/g' elementosh-GRAPE/places/{16,24,32,48,64,128}/*
+# sed -i 's/e7c591/cd9ef7/g' elementosh-GRAPE/places/{16,24,32,48,64,128}/*
+# sed -i 's/7e5514/452981/g' elementosh-GRAPE/places/{16,24,32,48,64,128}/*
+
+# sed -i 's/efdfc4/e4c6fa/g' elementosh-GRAPE/actions/{16,24,32,48,64}/*
+# sed -i 's/e7c591/cd9ef7/g' elementosh-GRAPE/actions/{16,24,32,48,64}/*
+# sed -i 's/7e5514/452981/g' elementosh-GRAPE/actions/{16,24,32,48,64}/*
+
+# sed -i 's/e9e9e9/e4c6fa/g' elementosh-GRAPE/apps/{16,24,32,48,64,128}/*
+# sed -i 's/b4b4b4/cd9ef7/g' elementosh-GRAPE/apps/{16,24,32,48,64,128}/*
+# sed -i 's/6c6c6c/452981/g' elementosh-GRAPE/apps/{16,24,32,48,64,128}/*
+
+cp -r 64 elementosh-GRAPE/apps
+gtk-update-icon-cache elementosh-GRAPE
+
+
+# @define-color COCOA_100 #a3907c;
+# @define-color COCOA_300 #8a715e;
+# @define-color COCOA_900 #3d211b;
+
+# sed -i 's/efdfc4/a3907c/g' elementosh-COCOA/places/{16,24,32,48,64,128}/*
+# sed -i 's/e7c591/8a715e/g' elementosh-COCOA/places/{16,24,32,48,64,128}/*
+# sed -i 's/7e5514/3d211b/g' elementosh-COCOA/places/{16,24,32,48,64,128}/*
+
+# sed -i 's/efdfc4/a3907c/g' elementosh-COCOA/actions/{16,24,32,48,64}/*
+# sed -i 's/e7c591/8a715e/g' elementosh-COCOA/actions/{16,24,32,48,64}/*
+# sed -i 's/7e5514/3d211b/g' elementosh-COCOA/actions/{16,24,32,48,64}/*
+
+# sed -i 's/e9e9e9/a3907c/g' elementosh-COCOA/apps/{16,24,32,48,64,128}/*
+# sed -i 's/b4b4b4/8a715e/g' elementosh-COCOA/apps/{16,24,32,48,64,128}/*
+# sed -i 's/6c6c6c/3d211b/g' elementosh-COCOA/apps/{16,24,32,48,64,128}/*
+
+cp -r 64 elementosh-COCOA/apps
+gtk-update-icon-cache elementosh-COCOA
+
+
+
+# @define-color SILVER_100 #fafafa;
+# @define-color SILVER_300 #d4d4d4;
+# @define-color SILVER_900 #555761;
+
+# sed -i 's/efdfc4/fafafa/g' elementosh-SILVER/places/{16,24,32,48,64,128}/*
+# sed -i 's/e7c591/d4d4d4/g' elementosh-SILVER/places/{16,24,32,48,64,128}/*
+# sed -i 's/7e5514/555761/g' elementosh-SILVER/places/{16,24,32,48,64,128}/*
+
+# sed -i 's/efdfc4/fafafa/g' elementosh-SILVER/actions/{16,24,32,48,64}/*
+# sed -i 's/e7c591/d4d4d4/g' elementosh-SILVER/actions/{16,24,32,48,64}/*
+# sed -i 's/7e5514/555761/g' elementosh-SILVER/actions/{16,24,32,48,64}/*
+
+# sed -i 's/e9e9e9/fafafa/g' elementosh-SILVER/apps/{16,24,32,48,64,128}/*
+# sed -i 's/b4b4b4/d4d4d4/g' elementosh-SILVER/apps/{16,24,32,48,64,128}/*
+# sed -i 's/6c6c6c/555761/g' elementosh-SILVER/apps/{16,24,32,48,64,128}/*
+
+cp -r 64 elementosh-SILVER/apps
+gtk-update-icon-cache elementosh-SILVER
+
+# @define-color SLATE_100 #95a3ab;
+# @define-color SLATE_300 #667885;
+# @define-color SLATE_900 #0e141f;
+
+# sed -i 's/efdfc4/95a3ab/g' elementosh-SLATE/places/{16,24,32,48,64,128}/*
+# sed -i 's/e7c591/667885/g' elementosh-SLATE/places/{16,24,32,48,64,128}/*
+# sed -i 's/7e5514/0e141f/g' elementosh-SLATE/places/{16,24,32,48,64,128}/*
+
+# sed -i 's/efdfc4/95a3ab/g' elementosh-SLATE/actions/{16,24,32,48,64}/*
+# sed -i 's/e7c591/667885/g' elementosh-SLATE/actions/{16,24,32,48,64}/*
+# sed -i 's/7e5514/0e141f/g' elementosh-SLATE/actions/{16,24,32,48,64}/*
+
+# sed -i 's/e9e9e9/95a3ab/g' elementosh-SLATE/apps/{16,24,32,48,64,128}/*
+# sed -i 's/b4b4b4/667885/g' elementosh-SLATE/apps/{16,24,32,48,64,128}/*
+# sed -i 's/6c6c6c/0e141f/g' elementosh-SLATE/apps/{16,24,32,48,64,128}/*
+
+cp -r 64 elementosh-SLATE/apps
+gtk-update-icon-cache elementosh-SLATE
+
+
+# @define-color BLACK_100 #666;
+# @define-color BLACK_300 #4d4d4d;
+# @define-color BLACK_900 #000;
+
+# sed -i 's/efdfc4/666/g' elementosh-BLACK/places/{16,24,32,48,64,128}/*
+# sed -i 's/e7c591/4d4d4d/g' elementosh-BLACK/places/{16,24,32,48,64,128}/*
+# sed -i 's/7e5514/000/g' elementosh-BLACK/places/{16,24,32,48,64,128}/*
+
+# sed -i 's/efdfc4/666/g' elementosh-BLACK/actions/{16,24,32,48,64}/*
+# sed -i 's/e7c591/4d4d4d/g' elementosh-BLACK/actions/{16,24,32,48,64}/*
+# sed -i 's/7e5514/000/g' elementosh-BLACK/actions/{16,24,32,48,64}/*
+
+# sed -i 's/e9e9e9/666/g' elementosh-BLACK/apps/{16,24,32,48,64,128}/*
+# sed -i 's/b4b4b4/4d4d4d/g' elementosh-BLACK/apps/{16,24,32,48,64,128}/*
+# sed -i 's/6c6c6c/000/g' elementosh-BLACK/apps/{16,24,32,48,64,128}/*
+
+cp -r 64 elementosh-BLACK/apps
+gtk-update-icon-cache elementosh-BLACK
